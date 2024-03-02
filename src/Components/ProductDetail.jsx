80729ea5cd98507ed8077122442f2b8fa71f8fce
Himanshu Verma <himu1622@gmail.com>
2024-03-02 17:21:44 +0530

    working with react redux

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const PrdouctDetail = () => {
+    const {productId} = useParams()
+    console.error(productId);
     return (
         <>
             <h1>PrdouctDetail Component</h1>
